b7ce01e11446a76806e7a993bf6cdc588dd30856
a8ana <a8anassis@gmail.com>
2024-02-21 14:29:00 +0200

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/Main.java
@@ -0,0 +1,78 @@
+package gr.aueb.cf.schoolapp;
+
+import gr.aueb.cf.schoolapp.model.Course;
+import gr.aueb.cf.schoolapp.model.Student;
+import gr.aueb.cf.schoolapp.model.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("school8PU");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+//        Teacher thanos = new Teacher();
+//        thanos.setFirstname("Thanos");
+//        thanos.setLastname("Androutsos");
+//
+//        Teacher kapetis = new Teacher();
+//        kapetis.setFirstname("Makis");
+//        kapetis.setLastname("Kapetis");
+//
+//        Teacher markos = new Teacher();
+//        markos.setFirstname("Markos");
+//        markos.setLastname("Karabatsis");
+//
+//        Course java = new Course();
+//        java.setTitle("JAVA");
+//        java.setDescription("Java Course");
+//
+//        Course sql = new Course();
+//        sql.setTitle("SQL");
+//        sql.setDescription("SQL Course");
+//
+//        thanos.addCourse(java);
+//        kapetis.addCourse(sql);
+//
+//        Student alice = new Student();
+//        alice.setFirstname("Alice");
+//        alice.setLastname("W");
+//
+//        alice.addCourse(java);
+//
+
+//        Student alice = em.find(Student.class, 6L);
+//        Course java = em.find(Course.class, 4L);
+//        alice.deleteCourse(java);
+
+//        TypedQuery<Object[]> query = em.createQuery("SELECT t, c FROM Teacher t JOIN t.courses c" , Object[].class);
+//        List<Object[]> results = query.getResultList();
+//
+//        for (Object[] result : results) {
+//            System.out.print(result[0]);
+//            System.out.println(result[1]);
+//        }
+
+        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t JOIN t.courses c WHERE c.title = :title AND t.lastname = :lastname", Teacher.class);
+        query.setParameter("title", "Java");
+        query.setParameter("lastname", "Androutsos");
+        List<Teacher> teachers = query.getResultList();
+
+        teachers.forEach(System.out::println);
+
+//
+
+
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+
+    }
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "teachers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String firstname;
+
+    @Column(length = 50)
+    private String lastname;
+
+    @OneToMany(mappedBy = "teacher")
+    @Getter(AccessLevel.PRIVATE)
+    private Set<Course> courses = new HashSet<>();
+
+    public Set<Course> getAllCourses() {
+        return Collections.unmodifiableSet(courses);
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+        course.setTeacher(this);
+    }
+}
